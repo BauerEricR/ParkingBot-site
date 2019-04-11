cc6cd56543dc68ba5a9b355d7bcf6f938034bb51
BauerEricR <32853412+BauerEricR@users.noreply.github.com>
2019-04-11 01:16:02 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-In order to look at the website you only need the 2 files in "Build" 
-All the other files and directories are just what gets synthesized into Build.
-
+"# ParkingBot-site" 
--- source/resources/Header.js
+++ source/resources/Header.js
@@ -1,16 +1,22 @@
 var React = require('react');
+var logo = require('./ParkingBotLogoSmall.jpg');
 
 function Header() {
 let styles = {
-  margin: '50px',
-  backgroundColor: 'green',
-  fontSize: '500%',
+  //margin: '50px',
+  backgroundColor: 'blue',
+  fontSize: '50%',
   textalign: 'center',
-  fontColor:'navyblue',
-  textAlign: 'center'
+  //fontColor:'navyblue',
+  //textAlign: 'center',
+  height: '20px'
 } ;
 return(
-<header style= {styles}> ParkingBot </header>
+  <div>
+<img src = {logo} alt = 'Logo'/>
+<header style= {styles}></header>
+</div>
+
 )
 }
 
